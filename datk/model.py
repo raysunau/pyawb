44ac519969a09422e8a7353c7c64243069bb3c06
Ray Sun <sunrui.ray@gmail.com>
2020-11-20 22:28:32 +1100

    add module func

--- datk/model.py
+++ datk/model.py
@@ -1,2 +1,38 @@
-import pandas as pd
+#%%
 import os
+import io
+import yaml
+
+import pandas as pd
+
+
+# %%
+class ModelTrainer:
+    """
+    To train a merchine learning model based on the input yaml config
+    """
+    RAND_SEED = 42
+    
+    def __init__(self
+                ,df_input
+                ,y=None
+                ,yaml_config=None) -> None:
+        
+        self.config = {}
+        self.df_input = df_input
+        self.x_train = None
+        self.x_test = None
+        self.y_train = None
+        self.y_test = None
+        
+    def _create_model(self, **kwargs):
+        pass
+    
+    def _save_model(self,model):
+        pass
+
+    def _load_model(self):
+        pass
+
+
+
