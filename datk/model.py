54c3a00dc41c5f5f181d8b6b2d899ae648263b8e
Ray Sun <sunrui.ray@gmail.com>
2020-11-22 18:06:43 +1100

    add tests and examples

--- datk/model.py
+++ datk/model.py
@@ -1,9 +1,12 @@
 #%%
+from logging import getLogger
 import os
-import io
-import yaml
 
+import yaml
+import logging
 import pandas as pd
+
+from datk.configs import configs
 
 
 # %%
@@ -12,19 +15,46 @@ class ModelTrainer:
     To train a merchine learning model based on the input yaml config
     """
     RAND_SEED = 42
-    
+    input_cmds = ('fit','evaluate','predict','experiment')
+    supported_types = ('regression', 'classification', 'clustering')
+    results_path = configs.get('results_path')  # path to the results folder
+    default_model_path = configs.get('default_model_path')  # path to the pre-fitted model
+    description_file = configs.get('description_file')  # path to the description.json file
+    evaluation_file = configs.get('evaluation_file')  # path to the evaluation.json file
+    prediction_file = configs.get('prediction_file')  # path to the predictions.csv
+    default_dataset_props = configs.get('dataset_props')  # dataset props that can be changed from the yaml file
+    default_model_props = configs.get('model_props')  # model props that can be changed from the yaml file
+    model = None
+
+
+
     def __init__(self
-                ,df_input
-                ,y=None
-                ,yaml_config=None) -> None:
+                ,*args, **kwargs) -> None:
         
-        self.config = {}
-        self.df_input = df_input
-        self.x_train = None
-        self.x_test = None
-        self.y_train = None
-        self.y_test = None
-        
+        self.data_path:str = kwargs.get('data_path')
+        self.df_input = kwargs.get('df_input') 
+        self.logfile = kwargs.get('logfile')
+
+        if self.logfile != None:
+            self._set_logger(log_file = self.logfile)
+        else:
+            self.logger = logging.getLogger(name=__name__)
+            self.logger.setLevel(logging.INFO)
+        self.logger.info(f"Entered kwargs: {kwargs}")
+
+
+    def _set_logger(self,
+                    log_file:str = 'Pydatoolkt_logger.log',
+                    log_level = logging.INFO):
+        logging.basicConfig(
+            format = '%(asctime)s:%(levelname)s:%(message)s',
+            filename=log_file,
+            filemode='w',
+            level = log_level
+        )
+        logging.info('Start logging to ' + log_file + ' ' + str(self.model))
+
+
     def _create_model(self, **kwargs):
         pass
     
---
+++ tests/test_basic.py
@@ -0,0 +1,19 @@
+
+#%%
+# from .context import datk
+import sys
+import os.path
+
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from datk.model import ModelTrainer
+
+#%%
+
+def test_init():
+    
+    tester = ModelTrainer(df_input="test")
+    assert tester.RAND_SEED == 42
+    assert tester.default_dataset_props.get('split').get('test_size') == 0.2
+# %%
